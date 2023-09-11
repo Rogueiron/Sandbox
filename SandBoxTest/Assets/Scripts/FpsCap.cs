17b6959c306ea553cf83ac861addd0ee8bf1da95
Rogueiron <112098171+Rogueiron@users.noreply.github.com>
2023-09-11 14:44:14 -0400

    added spin music and fps cap

---
+++ SandBoxTest/Assets/Scripts/FpsCap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsCap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
