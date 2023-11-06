9b77736cb23afa811444b5ec8ef44355f1214d73
Rogueiron <112098171+Rogueiron@users.noreply.github.com>
2023-11-06 14:52:32 -0500

    added basic storage

---
+++ SandBoxTest/Assets/Scripts/Interfaces/IBuilding.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBuilding
+{
+    int wood { get; set; }
+    int iron { get; set; }
+}
---
+++ SandBoxTest/Assets/Scripts/Storage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Storage : MonoBehaviour
+{
+    public static int WoodStorage = 30;
+    public static int IronStorage = 10;
+    public static int WaterStorage = 0;
+    public static int PopCapStorage = 0;
+    public static int researchStorage = 0;
+
+    private void Update()
+    {
+
+    }
+}
