00ff2790c0a12494a1a1d306f9fefbf32453e760
Bugcreeper <112098804+Bugcreeper@users.noreply.github.com>
2023-11-13 14:17:24 -0500

    added resource images

--- SandBoxTest/Assets/Scripts/Storage.cs
+++ SandBoxTest/Assets/Scripts/Storage.cs
@@ -7,10 +7,15 @@ public class Storage : MonoBehaviour
 {
     public static int WoodStorage = 30;
     public static int IronStorage = 20;
+    public static int WaterStorage = 50;
+    public static int CoalStorage = 100;
     public static int PopCapStorage = 0;
     public static int researchStorage = 0;
 
-    [SerializeField] TextMeshProUGUI storageText;
+    [SerializeField] TextMeshProUGUI storageTextWo;
+    [SerializeField] TextMeshProUGUI storageTextI;
+    [SerializeField] TextMeshProUGUI storageTextWa;
+    [SerializeField] TextMeshProUGUI storageTextC;
 
     private void Update()
     {
@@ -18,6 +23,9 @@ public class Storage : MonoBehaviour
     }
     private void UpdateStorageDisplay()
     {
-        storageText.text = "Wood: " + WoodStorage + "\nIron:" + IronStorage;
+        storageTextWo.text = WoodStorage.ToString();
+        storageTextI.text = IronStorage.ToString();
+        storageTextWa.text = WaterStorage.ToString();
+        storageTextC.text = CoalStorage.ToString();
     }
 }
