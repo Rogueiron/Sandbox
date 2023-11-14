5fa76d57d75a13c935a8e4fc3a6bb79efde1953e
Bugcreeper <112098804+Bugcreeper@users.noreply.github.com>
2023-11-14 14:45:15 -0500

    added adjustment for resource images(unfinished)

---
+++ SandBoxTest/Assets/Scripts/ResourceAdjustmentUI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static Storage;
+
+public class ResourceAdjustmentUI : MonoBehaviour
+{
+    [SerializeField] int resource;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // wood
+        if (resource == 0)
+        {
+            if (Storage.WoodStorage > 100)
+            {
+                
+            }
+        }
+        // Iron
+        else if(resource == 1)
+        {
+            if (Storage.IronStorage > 100)
+            {
+
+            }
+        }
+        // water
+        else if (resource == 2)
+        {
+            if (Storage.WaterStorage > 100)
+            {
+
+            }
+        }
+        // coal
+        else if (resource == 3)
+        {
+            if (Storage.CoalStorage > 300)
+            {
+
+            }
+        }
+    }
+}
