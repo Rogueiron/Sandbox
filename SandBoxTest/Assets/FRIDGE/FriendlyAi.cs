ec736f9725f67d01555ef93141d1a172eba87c37
Craby006-FCCC <amazingmincrabs@gmail.com>
2023-12-11 14:56:41 -0500

    Ai work

---
+++ SandBoxTest/Assets/FRIDGE/FriendlyAi.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FriendlyAi : MonoBehaviour
+{
+    public GameObject targetOBJ;
+
+    public NavMeshAgent agent;
+
+    public LayerMask whatIsGround, whatIsTarget;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public int strength;
+
+    //States
+    public float sightRange, attackRange;
+    public bool targetInSightRange, targetInAttackRange;
+    private void Start()
+    {
+        GetComponent<SphereCollider>().radius = sightRange;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsTarget);
+        targetInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsTarget);
+
+        if (!targetInSightRange && !targetInAttackRange)
+        {
+            targetOBJ = null;
+        }
+        if (targetInSightRange && !targetInAttackRange) ChaseTarget();
+        if (targetInSightRange && targetInAttackRange) AttackTarget();
+    }
+
+    private void ChaseTarget()
+    {
+        if (targetOBJ != null)
+        {
+            agent.SetDestination(targetOBJ.transform.position);
+        }
+    }
+
+    private void AttackTarget()
+    {
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(targetOBJ.transform);
+
+        if (!alreadyAttacked)
+        {
+            Debug.Log("ATTACKED");
+            targetOBJ.GetComponent<Stats>().health -= strength;
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+        Debug.Log("RELOADED");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Enemy") && targetOBJ == null)
+        {
+            targetOBJ = other.gameObject;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == targetOBJ)
+        {
+            targetOBJ = null;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+}
