f1f51a5582052063a6a96a635ee83cceecbf3c26
Bugcreeper <112098804+Bugcreeper@users.noreply.github.com>
2023-12-19 12:29:57 -0500

    end game stuffs

---
+++ SandBoxTest/Assets/Scripts/UI/EndGame.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+
+    void Update()
+    {
+        CheckEnd();
+    }
+
+    private void CheckEnd()
+    {
+        
+        if(GameObject.FindWithTag("TownCastle"))
+        {
+            SceneManager.LoadScene("FailedGame");
+        }
+    }
+}
